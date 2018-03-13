0ea7be7bdc399ff725f732ae451bc33606e9ead0
Doug Service <dougservice@apache.org>
2018-03-13 22:13:26 +0000

    Simple PInvoke/Delegate example
    This is the first pass of a simple program which
    demonstrates using PInvoke to call unmanaged code
    from managed code and delegates to call managed
    code from unmanaged code.

---
+++ CrossPITest/CrossPILibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrossPILibrary
+{
+    public class PIClass
+    {
+        public void DoIt()
+        {
+            Console.WriteLine("Doing It!!!!");
+        }
+    }
+}
---
+++ CrossPITest/CrossPITestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+using CrossPILibrary;
+
+namespace CrossPITestApp
+{
+    public static class Program
+    {
+        [DllImport("CrossPIDll.dll")]
+        private static extern void TheDllFunction();
+
+        delegate void TheDelegate();
+
+        [DllImport("CrossPIDll.dll")]
+        private static extern void TakeTheDelegate(TheDelegate del);
+
+        static void DelegateImpl()
+        {
+            Console.WriteLine("This is the delegate implementation speaking!");
+        }
+
+        static void Main(string[] args)
+        {
+            PIClass lib = new PIClass();
+            lib.DoIt();
+
+            TheDllFunction();
+            TakeTheDelegate(DelegateImpl);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
