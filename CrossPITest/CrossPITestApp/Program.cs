390dab17f3485845793ea5cfa6c80a0b4e6cec4b
Doug Service <dougservice@apache.org>
2018-03-25 00:06:11 +0000

    Interop with strings now works.

--- CrossPITest/CrossPITestApp/Program.cs
+++ CrossPITest/CrossPITestApp/Program.cs
@@ -8,16 +8,18 @@ namespace CrossPITestApp
     public static class Program
     {
         [DllImport("CrossPIDll.dll")]
-        private static extern void TheDllFunction();
+        private static extern void TheDllFunction([MarshalAs(UnmanagedType.LPWStr)] string str);
 
-        delegate void TheDelegate();
+        delegate void TheDelegate([MarshalAs(UnmanagedType.LPWStr)] string str);
 
         [DllImport("CrossPIDll.dll")]
         private static extern void TakeTheDelegate(TheDelegate del);
+        [DllImport("CrossPIDll.dll")]
+        private static extern void CallTheDelegate();
 
-        static void DelegateImpl()
+        static void DelegateImpl([MarshalAs(UnmanagedType.LPWStr)] string str)
         {
-            Console.WriteLine("This is the delegate implementation speaking!");
+            Console.WriteLine("This is the delegate implementation speaking! {0}", str);
         }
 
         static void Main(string[] args)
@@ -25,8 +27,9 @@ namespace CrossPITestApp
             PIClass lib = new PIClass();
             lib.DoIt();
 
-            TheDllFunction();
+            TheDllFunction("Parameter string");
             TakeTheDelegate(DelegateImpl);
+            CallTheDelegate();
             Console.WriteLine("Hello World!");
         }
     }
